3b06fa2eb7bbe2fbf58fd2ebfecdc9b2bb15ba29
Joonas <joonas.erkheikki@gmail.com>
2020-05-13 23:28:21 +0300

    Auto remove alert
    Auto remove alert after x seconds.

--- assets/js/angularjs/directives/alert/alert.controller.js
+++ assets/js/angularjs/directives/alert/alert.controller.js
@@ -1,9 +1,14 @@
 /*@ngInject*/
 export default class AlertController {
-    constructor($scope) {
+    constructor($scope, $timeout) {
         this.name = 'Alert Controller';
         this.$scope = $scope;
+        this.$timeout = $timeout;
+
+
         this.alerts = [];
+        // Time how long alert is showed.
+        this.alert_wait_time = 60000;
     }
 
     $onInit() {
@@ -14,11 +19,26 @@ export default class AlertController {
     }
 
     addAlert (msg) {
-        this.alerts.push({ 'message': msg});
+        let time = new Date();
+        let self = this;
+        this.alerts.push({ 'message': msg, 'created_time': time});
+
+
+        // Not real solution, but some kinda deletion for old alerts.
+        this.$timeout(function () {
+            let time = new Date();
+            time.setMilliseconds(time.getMilliseconds() - (self.alert_wait_time - 1000));
+
+            self.alerts.forEach(function (item,index) {
+                if(item.created_time < time){
+                    self.remove(index);
+                }
+            })
+        }, this.alert_wait_time);
     }
 
+    // Remove alert from array by index.
     remove(alert_index){
         this.alerts.splice(alert_index, 1);
     }
-
 }
